d4ee35f108533321f6499ce44435d74d920a8f03
axekan <axekan@live.se>
2025-07-24 19:08:31 +0200

    Add demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,36 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import DragHandle from "$lib/DragHandle.svelte";
+	import DragItem from "$lib/DragItem.svelte";
+	import DragList from "$lib/DragList.svelte";
+
+    let items = [
+        { id: '1', content: 'Item 1' },
+        { id: '2', content: 'Item 2' },
+        { id: '3', content: 'Item 3' },
+    ];
+</script>
+
+<h1>Svelte draglist</h1>
+
+<DragList {items} onReorder={(newItems) => items = newItems} gap="8px">
+    <DragItem>
+        {#snippet children(item)}
+            <div class="drag-item">
+                <DragHandle />
+                {item.content}
+            </div>
+        {/snippet}
+    </DragItem>
+</DragList>
+
+<style>
+    .drag-item {
+        height: 40px;
+        padding-left: 8px;
+        display: flex;
+        align-items: center;
+        gap: 6px;
+        border: 1px solid black;
+        background-color: white;
+    }
+</style>
